ce1714a0a5534d254d1c713e4a39256ae0097885
Pascal Iske <info@pascal-iske.de>
2019-04-06 17:38:14 +0200

    fix: simplify input types

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import * as Mime from 'mime/Mime'
 import * as standard from 'mime/types/standard.json'
 
-export interface TypeMap {
-    [key: string]: Array<string>
-}
-
 const instance = new Mime(standard)
 
 export const getType = (path: string): string => instance.getType(path)
 export const getExtension = (mime: string): string => instance.getExtension(mime)
-export const define = (mimes: TypeMap, force?: boolean): void => instance.define(mimes, force)
+export const define = (mimes: Record<string, string[]>, force?: boolean): void => {
+    return instance.define(mimes, force)
+}
