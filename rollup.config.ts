f22531b3ff7ca0e89340d69549c1492cfb0cf6a2
Pascal Iske <info@pascal-iske.de>
2019-04-24 14:45:44 +0200

    feat: bundle library for browser usage using umd format

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,18 +2,27 @@ import clear from 'rollup-plugin-clear'
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
 import { terser } from 'rollup-plugin-terser'
-import { module, main } from './package.json'
+import { module, main, browser } from './package.json'
 
 export default () => ({
     input: 'src/index.ts',
     output: [
         {
+            format: 'es',
+            file: module,
+        },
+        {
             format: 'cjs',
             file: main,
         },
         {
-            format: 'es',
-            file: module,
+            format: 'umd',
+            file: browser,
+            name: 'mimeTypes',
+            globals: {
+                tslib: 'tslib',
+                classnames: 'classNames',
+            },
         },
     ],
     external: ['mime/Mime', 'mime/types/standard.json'],
